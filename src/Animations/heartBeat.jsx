fb4cd9925514c51644163b155acfed191a5368c8
MichealGeorgeJ <michealmount23@gmail.com>
2024-08-14 21:48:55 +0530

    heartbeat backgroundcolor changed

--- src/Animations/heartBeat.jsx
+++ src/Animations/heartBeat.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const heartBeat = () => {
     return (
-        <div>
+        <div className='bigSphere'>
              <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
